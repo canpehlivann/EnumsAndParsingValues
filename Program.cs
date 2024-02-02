bfe66a95777b30e64eedfb5a94079879f481328d
Can Pehlivan <cpehlivan544@gmail.com>
2024-02-02 20:40:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EnumsAndParsingValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string response;
+            
+            bool success = false;
+            
+            int n = 0;
+
+            do
+            {
+                n++;
+                
+                try
+                {
+                    Console.Write("Please enter the current day of the week (spelled out fully):  ");
+                    
+                    response = Console.ReadLine();
+
+                    Week week = new Week();
+
+                    response = response.ToLower();
+                    
+                    response = week.createDay(response);
+                    
+                    var day = Enum.Parse<Week.Day>(response);
+
+                    Console.WriteLine($"We recognized your entry as : {day} ");
+
+                    success = true;
+
+                }
+
+                catch (ArgumentException)
+                {
+                    if (n < 3)
+                    {
+                        Console.WriteLine("Sorry, we didn't recognize your entry. Please use standard spelling.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Sorry, that didn't work out, either. We suggest running the program again and entering something like \"Tuesday\".");
+                    }
+                }
+            } while (!success && n < 3);
+              Console.Write("Please enter the current day of the week (spelled out fully):  ");
+        }
+    }
+}
---
+++ Week.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsAndParsingValues
+{
+    public class Week
+    {
+        public enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        };
+
+        public string createDay(string day)
+        {
+            
+            StringBuilder dayBuilder = new StringBuilder();
+            
+            dayBuilder.Append(day);
+            
+            dayBuilder[0] = dayBuilder.ToString().ToUpper()[0];
+            
+            return dayBuilder.ToString();
+        }
+    }
+}
